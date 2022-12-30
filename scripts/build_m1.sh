6aa3c5a1825bfeb1c55d42536eb5739d06267b97
Victor Neznaykin <va.neznaykin@gmail.com>
2022-12-30 21:20:45 +0400

    added dockerfile for build amd arch

--- scripts/build_m1.sh
+++ scripts/build_m1.sh
@@ -3,4 +3,7 @@
 set -ex
 cd `dirname $0`
 
-docker buildx build --platform linux/amd64 -t malekvictor/space-box-writer:latest --target=app ../
+docker buildx build --platform linux/amd64 -t malekvictor/space-box-writer:latest -f ../Dockerfile-amd --target=app ../
+#docker buildx create --use desktop-linux
+#docker buildx build --platform linux/arm64 -t malekvictor/space-box-writer:0.0.4 --load --target=app ../
+
