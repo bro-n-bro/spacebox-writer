e86cb2248a7505b575c1a721722ec47ade557e78
Реук Алексей Сергеевич (Reuk.Aleksey) <reuk.aleksey@svyaznoy.ru>
2023-01-15 17:00:12 +0300

    fixes

--- adapter/clickhouse/models/proposal_vote_message.go
+++ adapter/clickhouse/models/proposal_vote_message.go
@@ -3,7 +3,7 @@ package models
 type ProposalVoteMessage struct {
 	VoterAddress string `json:"voter"`
 	Option       string `json:"option"`
-	ProposalID   int64  `json:"proposal_id"`
+	ProposalID   uint64 `json:"proposal_id"`
 	Height       int64  `json:"height"`
 	MsgIndex     int64  `json:"msg_index"`
 }
