096981d02d823aaf0a3ba6ff32a4a9a15cc6e5a7
Victor Neznaykin <va.neznaykin@gmail.com>
2023-05-07 18:04:05 +0400

    fix lint

--- adapter/clickhouse/models/grant_allowance_message.go
+++ adapter/clickhouse/models/grant_allowance_message.go
@@ -5,12 +5,12 @@ import "database/sql"
 type (
 	// GrantAllowanceMessage represents grant allowance message
 	GrantAllowanceMessage struct {
+		Expiration sql.NullTime `json:"expiration"`
 		Granter    string       `json:"granter"`
 		Grantee    string       `json:"grantee"`
 		Allowance  string       `json:"allowance"`
 		TxHash     string       `json:"tx_hash"`
 		Height     int64        `json:"height"`
 		MsgIndex   int64        `json:"msg_index"`
-		Expiration sql.NullTime `json:"expiration"`
 	}
 )
