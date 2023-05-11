7aacb41fbf0905fc5270fe641d63744bebd3a2c8
Victor Neznaykin <va.neznaykin@gmail.com>
2023-05-11 23:59:46 +0400

    add migration

---
+++ adapter/clickhouse/migrations/000050_handle_validator_signature.up.sql
@@ -0,0 +1,11 @@
+-- 000050_handle_validator_signature.up.sql
+CREATE TABLE IF NOT EXISTS spacebox.handle_validator_signature
+(
+    `height`  Int64,
+    `address` String,
+    `power`   String,
+    `reason`  String,
+    `jailed`  String,
+    `coin`    json
+) ENGINE = ReplacingMergeTree()
+      ORDER BY (`height`, `address`);
